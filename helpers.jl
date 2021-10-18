f7c0c2fffc25d7275aefcd0bde9098858edbb126
Volodymyr <volodia23byk@gmail.com>
2021-10-18 14:11:37 +0000

    add lab_4

--- helpers.jl
+++ helpers.jl
@@ -1,5 +1,5 @@
 module Helpers
-using LinearAlgebra
+using LinearAlgebra, LightGraphs, GraphPlot, Colors
 
 create_realtions(A, B, f=tuple) = hcat([ [f(i, j)  for i in A] for j in B]...)
 
@@ -19,5 +19,57 @@ function colder_eq(a, b)
     true
 end
 
-export create_realtions, r, colder_eq
+function plot_graph(R::Matrix{Bool}, nodelabel = [])
+    g = SimpleDiGraph(R)
+    nodesize = [LightGraphs.outdegree(g, v) for v in LightGraphs.vertices(g)]
+    alphas = nodesize/maximum(nodesize)
+    nodefillc = [RGBA(0.9,0.7,0.7,i) for i in alphas*1.5]
+    if isempty(nodelabel)
+        nodelabel = [i for i in 1:size(R)[1]]
+    end
+    gplot(g,  layout=circular_layout, linetype="straight", nodelabel=nodelabel, nodefillc=nodefillc) |> display
+end
+
+#consistency_check(R1::Matrix{Bool}, R2::Matrix{Bool}) = @assert size(R1) == size(R2) "size(R1) must equal size(R2)" 
+consistency_check(R1, R2) = ()
+"""
+R1 = Bool[1 1 1 1; 0 1 1 1; 0 1 0 0; 1 0 1 0]
+R2 = Bool[0 1 0 1; 0 0 0 1; 0 1 0 0; 0 0 1 0]
+r_mult(R1, R2) |> display
+
+4×4 Matrix{Bool}:
+ 0  1  1  1
+ 0  1  1  1
+ 0  0  0  1
+ 0  1  0  1
+"""
+function matrix_op(relation)
+    return function r_union(R1, R2)
+        consistency_check(R1, R2)
+        new_R = copy(R1)
+        map(arg -> relation(R2[arg[1]], arg[2]), enumerate(new_R))
+    end
+end
+
+# добуток/композиція
+function r_mult(R1::Matrix{Bool}, R2::Matrix{Bool})
+    consistency_check(R1, R2)
+    r, c = size(R1)
+    R_new = zeros(Bool, r, c)
+    for i in 1:r, j in 1:c
+        R_new[i, j] = any(R1[i, :] .& R2[:, j])
+    end
+    R_new
+end
+    
+# об'єднання
+r_union = matrix_op(|)
+# перетин
+r_intersec = matrix_op(&)
+# обернене
+r_rotated(R::Matrix) = transpose(R)
+# доповнення
+r_inverse(R::Matrix) = map(!, R)
+
+export create_realtions, r, colder_eq, plot_graph, r_intersec, r_inverse, r_mult, r_union, r_rotated
 end
